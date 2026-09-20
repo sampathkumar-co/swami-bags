8e379ab965b4038fe8ef21e182ac57afcf943de5
sampathkumar-co <sk96429643@gmail.com>
2026-09-20 19:00:53 +0530

    Correct newline in AdminLogin.tsx

--- src/admin/AdminLogin.tsx
+++ src/admin/AdminLogin.tsx
@@ -1,5 +1,6 @@
 import { LockKeyhole, LogIn } from 'lucide-react'
-import { useEffect, useState } from 'react'\nimport type { FormEvent } from 'react'
+import { useEffect, useState } from 'react'
+import type { FormEvent } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { adminApi } from '../lib/adminApi'
 
