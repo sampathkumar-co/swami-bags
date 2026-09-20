01585b7c7e07467e3fbfaa9b644ecdbd78b5fc4b
sampathkumar-co <sk96429643@gmail.com>
2026-09-20 18:23:59 +0530

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:slug" element={<ProductDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  )
+}
